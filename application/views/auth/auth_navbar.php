62f4852a0c02560af2f2bf88758c5391f0205cc2
Daffa Muttaqin <nine@upi.edu>
2024-01-10 15:23:05 +0700

    mebambahkan halaman login

--- application/views/auth/auth_navbar.php
+++ application/views/auth/auth_navbar.php
@@ -34,7 +34,7 @@
     
   </head>
 
-  <body style="font-family: 'Rubik', sans-serif;">    
+  <body style="font-family: 'Rubik', sans-serif; background-color: #f3f3f3;">    
     
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light shadow-lg" style="background-color: #6477db">
@@ -73,40 +73,5 @@
     </nav>
     <!-- End Navbar -->
 
-    <!-- Modal Notification-->
-    <div class="modal fade" id="notificationModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Notification</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Modal Avatar-->
-    <div class="modal fade" id="avatarModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Profile</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
+    <div class="col-12 d-flex" style="height: 94px; background-color: #6477db">
     </div>
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,1 +1,38 @@
-<div>Login Page</div>
+<div class=""; style="height: 100px; width: 100%;">
+    <div class="" style="width: 100%;">
+
+        <div class="d-flex flex-row mt-5 mx-5 bg-white rounded">
+            <!-- Gambar -->
+            <div class="col-6">
+                <img src="https://media.discordapp.net/attachments/1182687255895232554/1194525811588468766/img_login.png?ex=65b0abda&is=659e36da&hm=43899fad8837fc870b1c477af4a18de7346483c1be9073be6fd0dd9754e142f3&=&format=webp&quality=lossless&width=563&height=718" style="width: 563px; height: 718px;" alt="">
+            </div>
+    
+            <!-- Form Login -->
+            <div class="col-6 mt-4 text-center item-content-center pe-5">
+                <span style="color: #6F0056; font-size: 35px;" class="">SMK NEGERI 1</span>
+                <br>
+                <span style="color: #1E35AD; font-size: 35px" class="">BUMI RATU NUBAN</span>
+                <br>
+                <hr>
+                <div style="color: #828282; font-size: 26px" class="">PLEASE LOGIN</div>
+                <hr>
+
+                <form action="">
+                    <input class="form-control form-control-lg" type="text" placeholder="Username" aria-label=".form-control-lg example">
+                    <input class="form-control form-control-lg mt-3" type="password" placeholder="Password" aria-label=".form-control-lg example">
+
+                    <button type="submit" class="btn btn-primary w-100 mt-5 py-3">Login</button>
+                </form>
+
+                <button type="submit" class="btn btn-primary w-100 mt-5 py-3">Don’t have an account? Register</button>
+
+
+                <form action="" method="post"></form>
+            </div>
+
+        </div>
+        <div class="pt-5">
+
+        </div>
+    </div>
+</div>
